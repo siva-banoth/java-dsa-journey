c10ffff8a2d6351eb9e804e2d631e892cf20e71a
siva-banoth <siva.banoth.95@gmail.com>
2026-07-23 18:52:20 +0530

    Refractor driving licence eligibility examples

---
+++ 00-Basics/ConditionalStatements/DrivingLicenceEligibilityUsingIfElse.java
@@ -0,0 +1,29 @@
+package ConditionalStatements;
+/*
+Take a number n as input representing age of a person. Print "eligible" if the person is Eligible
+for driving license else Print "not eligible".
+
+Input Format:
+The only line contains an integer n
+
+Constraints:
+1<= n <=100
+ */
+
+import java.util.Scanner;
+
+public class DrivingLicenceEligibilityUsingIfElse {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        if (n>=18) {
+            System.out.println("eligible");
+        }
+        else {
+            System.out.println("not eligible");
+        }
+
+    }
+}
--- 00-Basics/ConditionalStatements/DrivingLicenceEligibilityUsingSimpleIf.java
+++ 00-Basics/ConditionalStatements/DrivingLicenceEligibilityUsingSimpleIf.java
@@ -8,11 +8,10 @@ Read a person's age. If the age is 18 or above, print "You are Eligible".
 Otherwise, do nothing.
 */
 
-public class DrivingLicenceEligibility {
+public class DrivingLicenceEligibilityUsingSimpleIf {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the Age");
         int age = scanner.nextInt();
 
         if (age>=18) {
