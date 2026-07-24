bc94f8907b7f46ac3b1bf655247cf28dc9a0c8dc
siva-banoth <siva.banoth.95@gmail.com>
2026-07-24 15:53:33 +0530

    maximum of three numbers

---
+++ 00-Basics/ConditionalStatements/MaximumOfThreeNumbersIfElseLadder.java
@@ -0,0 +1,37 @@
+package ConditionalStatements;
+/*
+
+Problem :
+Take three numbers a,b and c as input. Print the max out of 3 Integers.
+
+Input Format:
+The first line contains integer a.
+The second line contains integer b.
+The third line contains integer c.
+
+Output Format:
+Output according to problem statement.
+
+Constraints:
+1<= a,b and c <=10000
+ */
+
+import java.util.Scanner;
+
+public class MaximumOfThreeNumbersIfElseLadder {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        if (a >= b && a >= c) {
+            System.out.println(a);
+        } else if (b >= a && b >= c) {
+            System.out.println(b);
+        } else {
+            System.out.println(c);
+        }
+    }
+}
---
+++ 00-Basics/Loops/PrintFactorsWhile.java
@@ -0,0 +1,27 @@
+package Loops;
+
+/*
+Problem :
+Take as input a number n. Print factors of n
+
+ */
+
+import java.util.Scanner;
+
+public class FactorsWhile {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int i = 1;
+        while (i <= n) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+
+
+    }
+}
