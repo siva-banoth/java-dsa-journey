4d90ffd5d027b3abc7f8cc83900eee3866276541
siva-banoth <siva.banoth.95@gmail.com>
2026-07-26 22:33:08 +0530

    Triangle Pattern

--- 00-Basics/PatternPrinting/Triangle.java
+++ 00-Basics/PatternPrinting/Triangle.java
@@ -1,4 +1,20 @@
 package PatternPrinting;
 
+import java.util.Scanner;
+
 public class Triangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int nst =  1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("*");
+            }
+            nst++;
+            System.out.println();
+        }
+    }
 }
