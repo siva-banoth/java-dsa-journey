94a2e399f04a26ff652fac8b1bea7f5012306b4f
siva-banoth <siva.banoth.95@gmail.com>
2026-07-26 22:08:32 +0530

    Rectangle Pattern

---
+++ 00-Basics/PatternPrinting/Rectangle.java
@@ -0,0 +1,19 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
