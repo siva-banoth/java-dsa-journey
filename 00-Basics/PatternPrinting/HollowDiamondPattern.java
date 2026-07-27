27b2e2a7f1931566715522220bb0ffde8c7335bd
siva-banoth <siva.banoth.95@gmail.com>
2026-07-27 15:42:18 +0530

    Hollow  Diamond pattern

---
+++ 00-Basics/PatternPrinting/HollowDiamondPattern.java
@@ -0,0 +1,38 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class HollowDiamondPattern {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int nsp = 1;
+        int nst = n / 2 + 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("*");
+
+            }
+            for (int k = 1; k <= nsp; k++) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= nst; l++) {
+                System.out.print("*");
+            }
+
+            if (i <= n / 2) {
+                nst = nst - 1;
+                nsp = nsp + 2;
+            } else {
+                nst = nst + 1;
+                nsp = nsp - 2;
+            }
+
+            System.out.println();
+        }
+
+    }
+}
