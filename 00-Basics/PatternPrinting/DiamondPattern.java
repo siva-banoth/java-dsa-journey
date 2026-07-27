92d14ed41fe99d213e453aaac581ea6312b8c7cd
siva-banoth <siva.banoth.95@gmail.com>
2026-07-27 14:09:57 +0530

    Diamond Pattern

---
+++ 00-Basics/PatternPrinting/DiamondPattern.java
@@ -0,0 +1,52 @@
+package PatternPrinting;
+
+/*
+
+Take as input a number n, the number of rows
+Print the following pattern
+
+  *
+ ***
+*****
+ ***
+  *
+
+for n = 5.
+
+Input Format: Integer
+Constraints : 1<=n<=25 and is and odd number
+
+ */
+
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner((System.in));
+        int n = input.nextInt();
+
+        int nsp = n / 2;
+        int nst = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= nst; k++) {
+                System.out.print("*");
+            }
+
+            if (i <= n / 2) {
+                nst = nst + 2;
+                nsp = nsp - 1;
+            } else {
+                nst = nst - 2;
+                nsp = nsp + 1;
+            }
+
+            System.out.println();
+        }
+
+    }
+}
