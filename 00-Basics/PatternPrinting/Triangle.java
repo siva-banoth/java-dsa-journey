5c10c52b52bfe8eeaa49353d18c62ed852de32de
siva-banoth <siva.banoth.95@gmail.com>
2026-07-27 11:26:43 +0530

    Triangle pattern

--- 00-Basics/PatternPrinting/Triangle.java
+++ 00-Basics/PatternPrinting/Triangle.java
@@ -11,7 +11,7 @@ public class Triangle {
         int nst =  1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= nst; j++) {
-                System.out.print("*");
+                System.out.print("*" + " ");
             }
             nst++;
             System.out.println();
